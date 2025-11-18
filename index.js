da1173ce198c02183f9600ef0ea81749122a704c
Kaczucha24 <krupakonrad24@gmail.com>
2025-11-18 08:26:02 +0000

    Com4_1.1

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ const app = express();
 // define endpoint for exercise 1 here
 app.get('/math/circle/:r', (req, res) => {
 //TODO1  
+  const r = parseFloat(req.params.r);
+
+  if (isNaN(r) || r < 0) {
+    return res.status(400).json({ error: "Invalid radius" });
+  }
+
+  const area = (Math.PI * r * r).toFixed(2);
+  const circumference = (2 * Math.PI * r).toFixed(2);
+
+  const result = {
+    area,
+    circumference
+  };
   res.json(result);
 });
 
@@ -15,7 +28,7 @@ app.get('/math/circle/:r', (req, res) => {
 //TODO3
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
